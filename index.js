7063ef64532c1678ce8e0278a8128f029e97eb5d
Danny Garcia <danielgarcia1812@gmail.com>
2018-08-06 19:09:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 document.getElementById("title").innerHTML=movies["Titanic"]["title"]
-document.getElementById("Directo").innerHTML=movies[]
-
+document.getElementById("Directo").innerHTML=movies[Terminator2]
 document.innerhtml=movies
