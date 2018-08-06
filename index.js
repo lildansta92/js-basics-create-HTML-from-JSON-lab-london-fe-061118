8d130a3b6f210aee412295acb496696e98872c7f
Danny Garcia <danielgarcia1812@gmail.com>
2018-08-06 19:50:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 document.getElementById("title").innerHTML=movies["Titanic"]["title"]
 document.getElementById("Director").innerHTML=movies["Titanic"]["director"]
-document.getElementById("Genre")
+document.getElementById("genre").innerhtml=movies[]
